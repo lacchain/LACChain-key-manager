491b4b9b5b5a9bacce2f314fe603f0229a3992aa
eum602 <eum602@gmail.com>
2023-04-22 13:32:20 -0500

    chore: remove controller from exported classes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-export { Secp256k1Controller } from './controllers/secp256k1.controller';
 export { Secp256k1Service } from './services/secp256k1.service';
 export { Secp256k1 } from './entities/secp256k1.entity';
